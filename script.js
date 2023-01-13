bf4034324816ca24c699b0a17d6e32852ef5799c
Ismael <ausuaismael@gmail.com>
2023-01-12 20:29:23 -0500

    weather api

--- script.js
+++ script.js
@@ -52,3 +52,34 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+fetch("https://api.weather.gov/gridpoints/IWX/48,75/forecast")
+        .then(response => response.json())
+        .then(data => {
+    // Get the forecast elements from the API response
+    const forecast = data.properties.periods;
+    // Create a container for the forecast
+    const forecastContainer = document.createElement("div");
+    forecastContainer.classList.add("forecast");
+    // Iterate over the forecast elements
+    forecast.forEach(day => {
+        if (forecast.indexOf(day)<=7) {
+          // Create a div for each day
+          const dayContainer = document.createElement("div");
+          dayContainer.classList.add("day");
+          // Add the date to the div
+          const date = document.createElement("p");
+          date.innerHTML = day.name;
+          dayContainer.appendChild(date);
+          // Add the temperature to the div
+          const temperature = document.createElement("p");
+          temperature.innerHTML = `${day.temperature}°F,  ${day.shortForecast}`;
+          dayContainer.appendChild(temperature);
+          // Add the day container to the forecast container
+          forecastContainer.appendChild(dayContainer);
+        }
+    });
+    // Add the forecast container to the HTML
+    const weather = document.getElementById("forecast");
+    weather.appendChild(forecastContainer);
+});
